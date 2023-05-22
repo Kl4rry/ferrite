4bbfe6b4ec7c2c254556a2436481306e75b2c5c3
Axel Kappel <69117984+Kl4rry@users.noreply.github.com>
2023-05-22 08:49:40 +0200

    Update lib.rs

--- ferrite-tree-sitter/src/lib.rs
+++ ferrite-tree-sitter/src/lib.rs
@@ -6,7 +6,7 @@ pub use tree_sitter_css;
 pub use tree_sitter_glsl;
 pub use tree_sitter_html;
 pub use tree_sitter_json;
-pub use tree_sitter_md;
+//pub use tree_sitter_md;
 pub use tree_sitter_python;
 pub use tree_sitter_rust;
 pub use tree_sitter_toml;
