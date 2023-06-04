958f25b0d26eeec5d085b0e2f035fdeac3c05463
Axel Kappel <69117984+Kl4rry@users.noreply.github.com>
2023-06-04 14:36:02 +0200

    fix linux compile error

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -7,8 +7,6 @@ use arboard::Clipboard;
 use once_cell::sync::OnceCell;
 
 static CLIPBOARD: OnceCell<Mutex<Option<Clipboard>>> = OnceCell::new();
-#[cfg(target_os = "linux")]
-static PRIMARY_CLIPBOARD: OnceCell<Mutex<Option<Clipboard>>> = OnceCell::new();
 static LOCAL_CLIPBOARD: OnceCell<Mutex<String>> = OnceCell::new();
 static IS_USING_LOCAL_CLIPBOARD: AtomicBool = AtomicBool::new(false);
 
@@ -23,24 +21,10 @@ pub fn init(local_clipboard: bool) {
     if CLIPBOARD.set(Mutex::new(Some(clipboard))).is_err() {
         IS_USING_LOCAL_CLIPBOARD.store(true, Ordering::SeqCst);
     }
-
-    #[cfg(target_os = "linux")]
-    {
-        if let Ok(clipboard) = Clipboard::new() {
-            use arboard::{GetExtLinux, LinuxClipboardKind, SetExtLinux};
-            let clipboard = SetExtLinux::clipboard(clipboard, LinuxClipboardKind::Primary);
-            let clipboard = GetExtLinux::clipboard(clipboard, LinuxClipboardKind::Primary);
-            let _ = PRIMARY_CLIPBOARD.set(Mutex::new(Some(clipboard)));
-        }
-    }
 }
 
 pub fn uninit() {
     *CLIPBOARD.get().unwrap().lock().unwrap() = None;
-    #[cfg(target_os = "linux")]
-    {
-        *PRIMARY_CLIPBOARD.get().unwrap().lock().unwrap() = None;
-    }
 }
 
 pub fn set_contents(text: impl Into<String>) {
@@ -73,15 +57,21 @@ pub fn get_contents() -> String {
 
 #[cfg(target_os = "linux")]
 pub fn set_primary(text: impl Into<String>) {
-    let mut clipboard = PRIMARY_CLIPBOARD.get().unwrap().lock().unwrap();
-    let _ = clipboard.as_mut().unwrap().set_text(&text.into());
+    use arboard::{SetExtLinux, LinuxClipboardKind, Set};
+    if let Some(clipboard) = CLIPBOARD.get() {
+        let mut clipboard = clipboard.lock().unwrap();
+        let _ = clipboard.as_mut().unwrap().set().clipboard(LinuxClipboardKind::Primary).set_text(&text.into());
+    }
 }
 
 pub fn get_primary() -> String {
     #[cfg(target_os = "linux")]
     {
-        let mut clipboard = PRIMARY_CLIPBOARD.get().unwrap().lock().unwrap();
-        clipboard.as_mut().unwrap().get_text().unwrap_or_default()
+        use arboard::{GetExtLinux, LinuxClipboardKind, Get};
+        if let Some(clipboard) = CLIPBOARD.get() {
+            let mut clipboard = clipboard.lock().unwrap();
+            clipboard.as_mut().unwrap().get().clipboard(LinuxClipboardKind::Primary).get_text().unwrap_or_default()
+        }
     }
     #[cfg(not(target_os = "linux"))]
     {
