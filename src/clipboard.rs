3e3ee69c899a0775c89d28998cbdb001494ded42
Axel Kappel <69117984+Kl4rry@users.noreply.github.com>
2023-06-04 14:19:20 +0200

    fix linux compile error

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -26,9 +26,10 @@ pub fn init(local_clipboard: bool) {
 
     #[cfg(target_os = "linux")]
     {
-        if let Ok(mut clipboard) = Clipboard::new() {
-            use arboard::{LinuxClipboardKind, SetExtLinux};
-            clipboard.clipboard(LinuxClipboardKind::Primary);
+        if let Ok(clipboard) = Clipboard::new() {
+            use arboard::{LinuxClipboardKind, SetExtLinux, GetExtLinux};
+            let clipboard = SetExtLinux::clipboard(clipboard, LinuxClipboardKind::Primary);
+            let clipboard = GetExtLinux::clipboard(clipboard, LinuxClipboardKind::Primary);
             let _ = PRIMARY_CLIPBOARD.set(Mutex::new(Some(clipboard)));
         }
     }
