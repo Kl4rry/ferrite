5cac5e22d571666eb9dad47e18bda3d092633d86
Axel Kappel <69117984+Kl4rry@users.noreply.github.com>
2024-12-21 13:12:56 +0100

    fix lint

--- crates/ferrite-gui/src/srgb.rs
+++ crates/ferrite-gui/src/srgb.rs
@@ -1,7 +1,7 @@
 pub fn srgb_to_linear(c: f32) -> f32 {
     if c <= 0.04045 {
-        return c / 12.92;
+        c / 12.92
     } else {
-        return ((c + 0.055) / 1.055).powf(2.4);
+        ((c + 0.055) / 1.055).powf(2.4)
     }
 }
