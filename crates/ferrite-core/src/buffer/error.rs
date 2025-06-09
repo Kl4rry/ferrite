c531e9bdd714df1ac350bc1ea5aba3235c6ab8b3
Axel Kappel <69117984+Kl4rry@users.noreply.github.com>
2025-06-09 16:46:28 +0200

    add file locking

--- crates/ferrite-core/src/buffer/error.rs
+++ crates/ferrite-core/src/buffer/error.rs
@@ -4,6 +4,7 @@ use std::{error::Error, fmt, io};
 pub enum BufferError {
     NoPathSet,
     Io(io::Error),
+    Errno(rustix::io::Errno),
 }
 
 impl fmt::Display for BufferError {
@@ -11,6 +12,7 @@ impl fmt::Display for BufferError {
         match self {
             Self::NoPathSet => writeln!(f, "Error no path set"),
             Self::Io(err) => err.fmt(f),
+            Self::Errno(err) => err.fmt(f),
         }
     }
 }
@@ -19,6 +21,7 @@ impl Error for BufferError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
             Self::Io(err) => Some(err),
+            Self::Errno(err) => Some(err),
             _ => None,
         }
     }
@@ -27,5 +30,11 @@ impl Error for BufferError {
 impl From<io::Error> for BufferError {
     fn from(value: io::Error) -> Self {
         Self::Io(value)
+    }
+}
+
+impl From<rustix::io::Errno> for BufferError {
+    fn from(value: rustix::io::Errno) -> Self {
+        Self::Errno(value)
     }
 }
--- crates/ferrite-core/src/buffer/write.rs
+++ crates/ferrite-core/src/buffer/write.rs
@@ -1,12 +1,12 @@
 use std::{
-    fs::OpenOptions,
+    fs::{File, OpenOptions},
     io::{BufWriter, Write},
     path::Path,
 };
 
 use encoding_rs::{CoderResult, Encoding};
 use ferrite_utility::{graphemes::RopeGraphemeExt, line_ending::LineEnding};
-use ropey::{Rope, RopeBuilder};
+use ropey::Rope;
 
 use super::error::BufferError;
 
@@ -16,66 +16,58 @@ pub fn write(
     rope: Rope,
     path: impl AsRef<Path>,
 ) -> Result<usize, BufferError> {
-    let path = path.as_ref().to_path_buf();
+    let mut file = OpenOptions::new().create(true).write(true).open(path)?;
+    rustix::fs::fcntl_lock(&file, rustix::fs::FlockOperation::LockExclusive)?;
+    let bytes_written = write_inner(encoding, line_ending, rope, BufWriter::new(&mut file))?;
+    rustix::fs::fcntl_lock(&file, rustix::fs::FlockOperation::Unlock)?;
+    Ok(bytes_written)
+}
+
+fn write_inner(
+    encoding: &'static Encoding,
+    line_ending: LineEnding,
+    rope: Rope,
+    mut file: BufWriter<&mut File>,
+) -> Result<usize, BufferError> {
     const BUFFER_SIZE: usize = 8192;
 
-    let mut file = BufWriter::new(
-        OpenOptions::new()
-            .create(true)
-            .truncate(true)
-            .write(true)
-            .open(path)?,
-    );
-
-    let mut output_rope = RopeBuilder::new();
+    let mut output_string = String::new();
     for line in rope.lines() {
         if line.get_line_ending().is_some() {
             for chunk in line.line_without_line_ending(0).chunks() {
-                output_rope.append(chunk);
+                output_string.push_str(chunk);
             }
-            output_rope.append(line_ending.as_str());
+            output_string.push_str(line_ending.as_str());
         } else {
             for chunk in line.chunks() {
-                output_rope.append(chunk);
+                output_string.push_str(chunk);
             }
             break;
         }
     }
-    let rope = output_rope.finish();
 
     let mut encoder = encoding.new_encoder();
     let mut buffer = [0u8; BUFFER_SIZE];
 
     let mut total_written = 0;
 
-    let mut write = |chunk: &str, last: bool| -> Result<(), BufferError> {
-        let mut remainder = chunk;
-        loop {
-            match encoder.encode_from_utf8(remainder, &mut buffer[total_written..], last) {
-                (CoderResult::OutputFull, read, written, _) => {
-                    remainder = &remainder[read..];
-                    total_written += written;
+    let mut remainder = output_string.as_str();
+    loop {
+        match encoder.encode_from_utf8(remainder, &mut buffer[total_written..], true) {
+            (CoderResult::OutputFull, read, written, _) => {
+                remainder = &remainder[read..];
+                total_written += written;
 
-                    file.write_all(&buffer[..total_written])?;
-                    total_written = 0;
-                }
-                (CoderResult::InputEmpty, _, written, _) => {
-                    total_written += written;
-                    if last {
-                        file.write_all(&buffer[..total_written])?;
-                    }
-                    break;
-                }
+                file.write_all(&buffer[..total_written])?;
+                total_written = 0;
+            }
+            (CoderResult::InputEmpty, _, written, _) => {
+                total_written += written;
+                file.write_all(&buffer[..total_written])?;
+                break;
             }
         }
-        Ok(())
-    };
-
-    for chunk in rope.chunks() {
-        write(chunk, false)?;
     }
-
-    write("", true)?;
 
     file.flush()?;
     file.get_mut().sync_all()?;
